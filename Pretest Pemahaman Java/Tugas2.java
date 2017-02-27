d464da7b5c92a4bfe199828375742402826d30a5
kevinryo <muhammadkevinryoma@gmail.com>
2017-02-27 12:39:59 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+    
+    public static void main(String []args){
+        String[] nama = {"Dzihan" , "Rona" , "Ryoma"};
+        
+        System.out.println("Mahasiswa 1 : "+nama[0]);
+        System.out.println("Mahasiswa 2 : "+nama[1]);
+        System.out.println("Mahasiswa 3 : "+nama[2]);
+     }
+}
