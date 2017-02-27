fe28be6a6a87f391da3ccbfcb4c337fdeb04120e
kevinryo <muhammadkevinryoma@gmail.com>
2017-02-27 12:38:52 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas4.java
@@ -0,0 +1,14 @@
+public class Tugas4{
+    
+    public static int Penjumlahan(int i, int j){
+        return i + j;
+    }
+    
+    public static void main(String []args){
+        
+        int jumlah = Penjumlahan(2, 3);
+        System.out.println(jumlah);
+        
+    }
+    
+}
