f0998201e15669db6dbd0d45f0117d742d37e0df
kevinryo <muhammadkevinryoma@gmail.com>
2017-02-27 12:37:06 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Kevin Ryoma";
+        String kota = "Jepara";
+        System.out.println("Perkenalkan Nama Saya "+nama+" Berasal dari Kota "+kota);
+     }
+}
